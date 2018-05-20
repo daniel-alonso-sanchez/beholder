dcffe138a2017a40f0e720c1cc3e99ad248c0d64
alonsosanchezd <alonsosanchezd@yahoo.com>
2018-05-20 21:39:06 +0200

    First commit

---
+++ applications-service/src/main/java/org/bringer/tools/beholder/applications/model/Application.java
@@ -0,0 +1,45 @@
+package org.bringer.tools.beholder.applications.model;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+@Entity
+@Table(name = "applications")
+public class Application implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8505532442027182368L;
+	@Id
+    private UUID id;
+	@NotBlank
+    private String name;
+	@NotBlank
+    private String url;
+	public UUID getId() {
+		return id;
+	}
+	public void setId(UUID id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+}
