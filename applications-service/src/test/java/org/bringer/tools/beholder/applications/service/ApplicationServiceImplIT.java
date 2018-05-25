49e943312fbf748c51f9813edff25c85d9f1f9e0
DALOSA01 <DALOSA01@CASER-5041.caser.local>
2018-05-25 12:56:50 +0200

    Added remove op's to integration testing

--- applications-service/src/test/java/org/bringer/tools/beholder/applications/service/ApplicationServiceImplIT.java
+++ applications-service/src/test/java/org/bringer/tools/beholder/applications/service/ApplicationServiceImplIT.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.assertNotNull;
 import java.util.List;
 
 import org.bringer.tools.beholder.applications.model.Application;
+import org.bringer.tools.beholder.applications.repository.ApplicationRepository;
 import org.bringer.tools.beholder.applications.test.IntegrationTest;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -29,14 +30,19 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class ApplicationServiceImplIT extends BaseApplicationTest{
 	@Autowired
 	private IApplicationService service;
-	
+	@Autowired ApplicationRepository repo;
 	@Test
 	public void should_AddApplication_WhenFilleWithRightData() {
 		service.add(generateDefaultApplication());
 		List<Application>apps=service.findByName(DEFAULT_NAME);
 		assertNotNull(apps);	
 		assertNotEquals(0,apps.size());
-		assertEquals(DEFAULT_NAME, apps.get(0).getName());
+		Application app=apps.get(0);
+		assertEquals(DEFAULT_NAME, app.getName());
+		repo.delete(app.getId());
+		apps=service.findByName(DEFAULT_NAME);
+		assertNotNull(apps);	
+		assertEquals(0,apps.size());
 	}
 
 }
